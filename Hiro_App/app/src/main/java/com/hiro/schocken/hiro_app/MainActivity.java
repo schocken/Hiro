8db0ca47f1a985daaf5469cefe7011f8f1ab2e15
Eyal Schocken <schocken.eyal@gmail.com>
2018-03-23 11:17:29 +0300

    first commit

---
+++ Hiro_App/app/src/main/java/com/hiro/schocken/hiro_app/MainActivity.java
@@ -0,0 +1,18 @@
+package com.hiro.schocken.hiro_app;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Intent intent = new Intent(this, SensorService.class);
+        //Start Service
+        startService(intent);
+    }
+
+}
---
+++ Hiro_App/app/src/main/java/com/hiro/schocken/hiro_app/SensorService.java
@@ -0,0 +1,80 @@
+package com.hiro.schocken.hiro_app;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Handler;
+import android.os.IBinder;
+import android.support.v4.app.NotificationCompat;
+
+public class SensorService extends Service implements SensorEventListener {
+
+    private SensorManager mSensorManager;
+    private Sensor mAccelerometer;
+    private float mAccel; // acceleration apart from gravity
+    private float mAccelCurrent; // current acceleration including gravity
+    private float mAccelLast; // last acceleration including gravity
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mAccelerometer = mSensorManager
+                .getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mSensorManager.registerListener(this, mAccelerometer,
+                SensorManager.SENSOR_DELAY_UI, new Handler());
+        return START_STICKY;
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        float x = event.values[0];
+        float y = event.values[1];
+        float z = event.values[2];
+        mAccelLast = mAccelCurrent;
+        mAccelCurrent = (float) Math.sqrt((double) (x * x + y * y + z * z));
+        float delta = mAccelCurrent - mAccelLast;
+        mAccel = mAccel * 0.9f + delta; // perform low-cut filter
+
+        if (mAccel > 11) {
+            showNotification();
+        }
+    }
+
+    /**
+     * show notification when Accel is more then the given int.
+     */
+    private void showNotification() {
+        final NotificationManager mgr = (NotificationManager) this
+                .getSystemService(Context.NOTIFICATION_SERVICE);
+        NotificationCompat.Builder note = new NotificationCompat.Builder(this);
+        note.setContentTitle("Device Accelerometer Notification");
+        note.setTicker("New Message Alert!");
+        note.setAutoCancel(true);
+        // to set default sound/light/vibrate or all
+        note.setDefaults(Notification.DEFAULT_ALL);
+        // Icon to be set on Notification
+        note.setSmallIcon(R.drawable.ic_launcher);
+        // This pending intent will open after notification click
+        PendingIntent pi = PendingIntent.getActivity(this, 0, new Intent(this,
+                MainActivity.class), 0);
+        // set pending intent to notification builder
+        note.setContentIntent(pi);
+        mgr.notify(101, note.build());
+    }
+}
